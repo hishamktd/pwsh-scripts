cb5db2dc34f3e42eff54c480565842deb1ec19c1
Hisham PP <mohammedhisham@kiebot.com>
2025-05-25 15:29:14 +0530

    Add show-tree function to display directory structure with indentation and exclusion options

--- utility-scripts/main.ps1
+++ utility-scripts/main.ps1
@@ -7,3 +7,5 @@
 . C:\Users\User\PowerShell\custom-scripts\utility-scripts\help.ps1
 
 . C:\Users\User\PowerShell\custom-scripts\utility-scripts\count.ps1
+
+. C:\Users\User\PowerShell\custom-scripts\utility-scripts\show-tree.ps1
